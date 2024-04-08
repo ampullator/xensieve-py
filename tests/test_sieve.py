a52c32c684eba15c04a652391efe032c941b8ea2
Christopher Ariza <ariza@flexatone.com>
2024-04-08 15:46:24 -0700

    extend Python tests

--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -1,12 +1,17 @@
 from xensieve import Sieve
 
 
-def test_sieve_a() -> None:
+def test_sieve_repr_a() -> None:
     s = Sieve("3@2")
     assert str(s) == "Sieve{3@2}"
 
+def test_sieve_contains_a() -> None:
+    s1 = Sieve("5@0")
+    assert 5 in s1
+    assert 10 in s1
+    assert 15 in s1
 
-def test_sieve_b() -> None:
+def test_sieve_xor_a() -> None:
     s1 = Sieve("3@2")
     s2 = Sieve("5@1")
     s3 = s1 ^ s2
