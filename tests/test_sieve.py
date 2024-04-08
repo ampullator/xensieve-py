ceb06e0e50fe9a8f50b2e2adf134753985655ef3
Christopher Ariza <ariza@flexatone.com>
2024-04-08 16:02:46 -0700

    more python tests

--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -11,11 +11,29 @@ def test_sieve_contains_a() -> None:
     assert 10 in s1
     assert 15 in s1
 
+def test_sieve_invert_a() -> None:
+    s1 = Sieve("5@0")
+    s2 = ~s1
+    assert str(s3) == "Sieve{!5@0}"
+    assert 4 in s1
+
 def test_sieve_xor_a() -> None:
     s1 = Sieve("3@2")
     s2 = Sieve("5@1")
     s3 = s1 ^ s2
     assert str(s3) == "Sieve{3@2^5@1}"
+
+def test_sieve_or_a() -> None:
+    s1 = Sieve("3@2")
+    s2 = Sieve("5@1")
+    s3 = s1 | s2
+    assert str(s3) == "Sieve{3@2|5@1}"
+
+def test_sieve_and_a() -> None:
+    s1 = Sieve("3@2")
+    s2 = Sieve("5@1")
+    s3 = s1 & s2
+    assert str(s3) == "Sieve{3@2&5@1}"
 
 
 
